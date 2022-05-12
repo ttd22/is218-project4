9d776b0af10fa323e64811a3361fc9578be27cae
trangttdang <trangttdang01@gmail.com>
2022-05-11 22:50:38 -0400

    added config and init app

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,77 @@
 """A simple flask web app"""
+import logging
 import os
-from flask import Flask, render_template
-from app.cli import create_database
+
+import flask_login
+from flask import Flask
+from flask_bootstrap import Bootstrap5
+from flask_cors import CORS
+from flask_mail import Mail
+from flask_wtf.csrf import CSRFProtect
+
+from app.auth import auth
+from app.cli import create_database, create_log_folder
+from app.context_processors import utility_text_processors
+from app.db import database
 from app.db import db
 from app.db.models import User
+from app.error_handlers import error_handlers
+from app.logging_config import log_con, LOGGING_CONFIG
+from app.map import map
+from app.simple_pages import simple_pages
+from app.songs import songs
 
+mail = Mail()
 
-def page_not_found(e):
-    return render_template("404.html"), 404
+login_manager = flask_login.LoginManager()
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
-    app.secret_key = 'This is an INSECURE secret!! DO NOT use this in production!!'
+    if app.config["ENV"] == "production":
+        app.config.from_object("app.config.ProductionConfig")
+    elif app.config["ENV"] == "development":
+        app.config.from_object("app.config.DevelopmentConfig")
+    elif app.config["ENV"] == "testing":
+        app.config.from_object("app.config.TestingConfig")
 
-    app.register_error_handler(404, page_not_found)
-    db_dir = "database/db.sqlite"
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(db_dir)
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.init_app(app)
+    app.mail = Mail(app)
+    # https://flask-login.readthedocs.io/en/latest/  <-login manager
+    login_manager.init_app(app)
+    login_manager.login_view = "auth.login"
+
+    csrf = CSRFProtect(app)
+    bootstrap = Bootstrap5(app)
+
+    # these load functions with web interface
+    app.register_blueprint(simple_pages)
+    app.register_blueprint(auth)
+    app.register_blueprint(database)
+
+    # these load functionality without a web interface
+    app.register_blueprint(log_con)
+    app.register_blueprint(error_handlers)
+
+    app.register_blueprint(songs)
+    app.register_blueprint(map)
+    app.context_processor(utility_text_processors)
+
     # add command function to cli commands
     app.cli.add_command(create_database)
+    app.cli.add_command(create_log_folder)
 
-    @app.route('/')
-    def hello():
-        return 'Hello, World!'
-
+    # app.add_url_rule("/", endpoint="index")
+    db.init_app(app)
+    api_v1_cors_config = {
+        "methods": ["OPTIONS", "GET", "POST"],
+    }
+    CORS(app, resources={"/api/*": api_v1_cors_config})
     return app
+
+
+@login_manager.user_loader
+def user_loader(user_id):
+    try:
+        return User.query.get(int(user_id))
+    except:
+        return None
