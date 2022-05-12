519f8b352e785d507777836e18b795686d7dd4e5
trangttdang <trangttdang01@gmail.com>
2022-05-11 23:46:36 -0400

    added map

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,7 @@ from app.db import db
 from app.db.models import User
 from app.error_handlers import error_handlers
 from app.logging_config import log_con, LOGGING_CONFIG
-# from app.map import map
+from app.map import map
 from app.simple_pages import simple_pages
 
 
@@ -53,7 +53,7 @@ def create_app():
     app.register_blueprint(error_handlers)
 
     # app.register_blueprint(songs)
-    # app.register_blueprint(map)
+    app.register_blueprint(map)
     app.context_processor(utility_text_processors)
 
     # add command function to cli commands
